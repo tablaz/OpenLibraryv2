096df2806571b0312177d8c8dd428d8dafff9da9
Ricardo Restrepo M <rrestrepo@seeketing.com>
2016-03-02 22:04:05 +0000

    End

--- openLibraryBooks/DetailViewController.swift
+++ openLibraryBooks/DetailViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    
+    
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
+    @IBOutlet weak var coverImage: UIImageView!
+    
+    
+    @IBOutlet weak var authorsList: UITextView!
+    
+    
 
-    var detailItem: AnyObject? {
+    var detailItem: Book? {
+
         didSet {
             // Update the view.
             self.configureView()
@@ -24,8 +33,19 @@ class DetailViewController: UIViewController {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
             if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+                label.text = detail.title
             }
+            if let cover = self.coverImage{
+                cover.image = detail.cover
+            }
+            if let authors = self.authorsList{
+                var authorsList: String = "Authors:\n"
+                for author in detail.authors {
+                    authorsList = authorsList+"\(author)\n"
+                }
+                authors.text = authorsList as String
+            }
+
         }
     }
 
--- openLibraryBooks/MasterViewController.swift
+++ openLibraryBooks/MasterViewController.swift
@@ -8,18 +8,126 @@
 
 import UIKit
 
+struct Book {
+    var title : String
+    var cover : UIImage
+    var authors : [String]
+    
+    
+    init(title: String, cover : UIImage, authors : [String]){
+        self.title = title
+        self.cover = cover
+        self.authors = authors
+    }
+}
+
+
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
     var objects = [AnyObject]()
+    var booksStruc = [Book]()
 
+    @IBAction func bookSearch(sender: UITextField) {
+        let bookResult : Book = searchOpenLibraryByISBN(sender)
+        booksStruc.append(bookResult)
+        self.tableView!.reloadData()
+        sender.text = ""
+        sender.resignFirstResponder()
+        
+        /* Trying to push the detail view programatically
+        let lastSectionIndex = self.tableView.numberOfSections-1
+        let lastSectionLastRow = self.tableView.numberOfRowsInSection(lastSectionIndex) - 1
+        let indexPath = NSIndexPath(forRow:lastSectionLastRow, inSection: lastSectionIndex)
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        print(cell?.textLabel?.text)
+        
+        self.performSegueWithIdentifier("showDetail", sender: cell)
+        */
 
+    }
+    @IBOutlet weak var bookSearch: UITextField!
+
+    
+    
+    func searchOpenLibraryByISBN(sender: UITextField) -> Book {
+        var books : Book = Book(title: "", cover: UIImage() , authors: [])
+        
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(sender.text!)"
+        let safeURL = urls.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        let url = NSURL(string: safeURL)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        let texto = NSString(data:datos!, encoding: NSUTF8StringEncoding)
+        if texto == "{}" || texto == "" {
+            let alerta = UIAlertController(title: "Search Result", message: "No Data Found", preferredStyle: UIAlertControllerStyle.Alert)
+            alerta.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+            self.presentViewController(alerta, animated: true, completion: nil)
+            sender.text = "";
+        } else if texto == nil {
+            let alerta = UIAlertController(title: "Search Result", message: "No Data Found, check Internet connection", preferredStyle: UIAlertControllerStyle.Alert)
+            alerta.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+            self.presentViewController(alerta, animated: true, completion: nil)
+            sender.text = "";
+        } else {
+            books =  self.parceBooksJson(datos!)
+            
+        }
+        
+        return books
+    }
+    
+    
+    
+    func parceBooksJson(nsdata: NSData) -> Book {
+        var bookDoc : Book = Book(title: "", cover: UIImage() , authors: [])
+        
+        do {
+            
+            let jsonFull = try NSJSONSerialization.JSONObjectWithData(nsdata, options: NSJSONReadingOptions.MutableContainers) as! [String: AnyObject]
+            let bookArray = jsonFull as NSDictionary
+            for (_, value) in bookArray {
+                // Process Book Title
+                let title  = value["title"] as! String
+
+                // Process Author List
+                let authors = value["authors"]! != nil ? value["authors"] as! NSArray : []
+                var authorsArray : [String] = [String]()
+                for author in authors {
+                    let authorName = author["name"] as! String
+                    authorsArray.append(authorName)
+                }
+                
+                // Process Book Cover
+                var cover : UIImage = UIImage(named: "missingbook.png")!
+                
+                if value["cover"] != nil {
+                    let imageUrls = value["cover"]??["large"]
+                    if imageUrls != nil {
+                        let url = NSURL(string: imageUrls as! String)
+                        let data = NSData(contentsOfURL: url!)
+                        cover = UIImage(data: data!)!
+                    }
+                }
+                print(cover)
+                
+                bookDoc = Book(title: title, cover: cover , authors: authorsArray)
+            }
+            
+            
+        } catch {
+            print("Error")
+
+        }
+        
+        return bookDoc
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        
+         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "enableBookSearch:")
+        //let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
             let controllers = split.viewControllers
@@ -27,6 +135,11 @@ class MasterViewController: UITableViewController {
         }
     }
 
+    func enableBookSearch(sender: AnyObject) {
+        self.bookSearch.hidden = false
+        self.bookSearch.resignFirstResponder()
+    }
+    
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
@@ -36,19 +149,19 @@ class MasterViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+/*
     func insertNewObject(sender: AnyObject) {
         objects.insert(NSDate(), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
-
+*/
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = booksStruc[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
@@ -64,20 +177,20 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        //return objects.count
+        return booksStruc.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        cell.textLabel!.text = booksStruc[indexPath.row].title
         return cell
+        
     }
 
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
